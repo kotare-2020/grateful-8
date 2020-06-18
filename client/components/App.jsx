8499a688f6d6ade598e52dd8e51ef811a2bf2ba2
Joe Butler <joefish.b@gmail.com>
2020-06-19 11:46:18 +1200

    get background werkin

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,7 @@ import getRandomAnswer from '../../server/js/getRandomAnswer'
 
 import MyForm from "./Form"
 import Fate from "./Fate"
+import Hexgrid from "./Hexgrid"
 
 class App extends React.Component {
   state = {
@@ -20,18 +21,20 @@ class App extends React.Component {
     return (
       <>
         <Router>
-          <h1>Grateful 8</h1>
           <div className="container">
-          <MyForm setQuestion={this.setQuestion} />
-          <h1>{randomAnswer}</h1>
-            
-          <Route exact path="/Fate" component={Fate} />
+            <Hexgrid />
+            <div className="main">
+              <h1>Grateful 8</h1>
+              <MyForm setQuestion={this.setQuestion} />
+              <h1>{randomAnswer}</h1>
 
-          {/* <Route exact path="/" component={Home} /> */}
+              <Route exact path="/Fate" component={Fate} />
 
-          {/* Do we still need/want this  if not we can get rid of a lot of code */}
-          <h1>Your fate is: {this.state.question}</h1>
+              {/* <Route exact path="/" component={Home} /> */}
 
+              {/* Do we still need/want this  if not we can get rid of a lot of code */}
+              <h1>Your fate is: {this.state.question}</h1>
+            </div>
           </div>
         </Router>
       </>
---
+++ client/components/Hexgrid.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HexGrid, Layout, Hexagon, GridGenerator } from 'react-hexgrid';
+
+class Hexgrid extends React.Component {
+  render() {
+    const hexagons = GridGenerator.parallelogram(-15, 15, -15, 15)
+
+    return (
+      <div className="hexgrid">
+        <HexGrid width={window.innerWidth} height={window.innerHeight}>
+          <Layout size={{ x: 5, y: 5 }}>
+            { hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          </Layout>
+        </HexGrid>
+      </div>
+    );
+  }
+}
+
+export default Hexgrid
