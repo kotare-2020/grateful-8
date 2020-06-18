a3503b0041613f35779c2a40918ce420c132b6be
benjamin-means <ben.w.means@gmail.com>
2020-06-18 20:31:28 +1200

    Add route for Fate

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react"
+import { HashRouter as Router, Route, Link } from "react-router-dom"
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import MyForm from "./Form"
+import Fate from "./Fate"
+
+class App extends React.Component {
+  state = {
+    question: "",
+  }
+
+  setQuestion = (question) => {
+    this.setState({ question: question })
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <h1>React development has begun!</h1>
+
+          <MyForm setQuestion={this.setQuestion} />
+
+          <Route exact path="/Fate" component={Fate} />
+
+          {/* <Route exact path="/" component={Home} /> */}
+
+          {/* Do we still need/want this  if not we can get rid of a lot of code */}
+          <h1>Your fate is: {this.state.question}</h1>
+        </Router>
+      </>
+    )
+  }
 }
 
 export default App
