619b484f1aca2dd431221af5ca6e924903f68661
fai-songprasit <prapawan.songprasit@gmail.com>
2020-06-18 18:05:09 +1200

    create skeleton EightBall component

---
+++ client/components/EightBall.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const EightBall = props => {
+
+    //need a formula to change class in <p> and content
+
+    return (
+        <>
+        <h1>Say some clever things here</h1>
+        <div>
+            <img/>
+            <div>
+                <img/>
+                <div>
+                    <img/>
+                    <p class="big8">8</p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default EightBall
