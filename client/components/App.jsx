e182555141d945ccf41bb2428990bd6189682afb
benjamin-means <ben.w.means@gmail.com>
2020-06-19 12:30:21 +1200

    fix clash

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route, Link } from "react-router-dom"
 
 import MyForm from "./Form"
 import Hexgrid from "./Hexgrid"
-import Home from './Home'
+import Home from "./Home"
 import EightBall from "./EightBall"
 
 class App extends React.Component {
@@ -11,7 +11,7 @@ class App extends React.Component {
     question: "",
   }
 
-  setQuestion = question => this.setState({ question: question })
+  setQuestion = (question) => this.setState({ question: question })
 
   shootFire = () => {}
 
@@ -24,12 +24,13 @@ class App extends React.Component {
         <Router>
           <div className="container">
             <Hexgrid />
-            <div className="main">
-            <MyForm setQuestion={this.setQuestion} />
+            <div>
+              <div className="main">
+                <MyForm setQuestion={this.setQuestion} />
+              </div>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/Fate" component={EightBall} />
-              
+              <Route exact path="/" component={Home} />
+              <Route path="/Fate" component={EightBall} />
             </div>
             {/* <div onDragEnter={this.shootFire} >Shoot Fire animation</div> */}
             {/* <h1>Your fate is: {this.state.question}</h1> */}
@@ -42,5 +43,6 @@ class App extends React.Component {
 
 export default App
 
-
-{/* <iframe src="https://giphy.com/embed/6wpHEQNjkd74Q" width="480" height="253" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/6wpHEQNjkd74Q">via GIPHY</a></p> */}
+{
+  /* <iframe src="https://giphy.com/embed/6wpHEQNjkd74Q" width="480" height="253" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/6wpHEQNjkd74Q">via GIPHY</a></p> */
+}
