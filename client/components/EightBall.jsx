0b87aca002e878d575348974cb93b6c27206c704
fai-songprasit <prapawan.songprasit@gmail.com>
2020-06-19 14:51:07 +1200

    add fireworks after answer

--- client/components/EightBall.jsx
+++ client/components/EightBall.jsx
@@ -1,20 +1,26 @@
 import React from "react"
-
 import getRandomAnswer from "../../server/js/getRandomAnswer"
 
 class EightBall extends React.Component {
   render() {
+
     let randomAnswer = getRandomAnswer().answer
+    console.log(randomAnswer)
 
     return (
       <>
+      {randomAnswer && 
+      <div class="pyro">
+        <div class="before"></div>
+        <div class="after"></div>
+      </div>}
         <div className="backball">
           <div className="center_black">
             <div className="center_white">
-            <svg height="250" width="240" className="content">
+            <svg height="250" width="270" className="content">
                 <polygon points="10,10 120,230 230,10" className="triangle" />
                 <text
-                  x="50%"
+                  x="45%"
                   y="40%"
                   dominantBaseline="middle"
                   textAnchor="middle"
