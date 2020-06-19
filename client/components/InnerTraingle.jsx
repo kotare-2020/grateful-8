645f528ad9cb328b2d376cb720dd48790eb7e324
fai-songprasit <prapawan.songprasit@gmail.com>
2020-06-19 12:13:45 +1200

    CSS update

--- client/components/InnerTraingle.jsx
+++ client/components/InnerTraingle.jsx
@@ -5,7 +5,9 @@ const InnerTraingle = props => {
         <>
         <svg height="220" width="220" class="content">
             <polygon points="10,10 110,210 210,10" class="triangle" />
-            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">Some more stuff to do</text>    
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">
+                Some more stuff to do
+            </text>    
         </svg>
         </>
     )
