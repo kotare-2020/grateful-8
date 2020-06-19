ca9312f119b385b522d419d44f690d28853b9b74
Joe Butler <joefish.b@gmail.com>
2020-06-19 14:30:00 +1200

    Get triangle fade in on submit

--- client/components/EightBall.jsx
+++ client/components/EightBall.jsx
@@ -12,13 +12,13 @@ class EightBall extends React.Component {
           <div className="center_black">
             <div className="center_white">
             <svg height="250" width="240" className="content">
-                <polygon points="10,10 120,230 230,10" className="triangle" />
+                <polygon points="10,10 120,230 230,10" className="triangle fade-in" />
                 <text
                   x="50%"
                   y="40%"
                   dominantBaseline="middle"
                   textAnchor="middle"
-                  className="text_color"
+                  className="text_color fade-in"
                 >
                   {randomAnswer}
                 </text>
